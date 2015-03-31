3fd2cd9bc18c7b8ec7bdf02cf9292454cb40f9f4
fjruizruano <fjruizruano@ugr.es>
2015-03-31 14:10:44 +0200

    quickmito added to mitobim_run

--- mitobim_run.py
+++ mitobim_run.py
@@ -5,7 +5,7 @@ import os
 from subprocess import call
 from Bio import SeqIO
 
-print "Usage: mitobim_run.py NumberOfReads ListOfFiles Reference"
+print "Usage: mitobim_run.py NumberOfReads ListOfFiles Reference [miramito/quickmito]"
 
 try:
     nreads = sys.argv[1]
@@ -22,9 +22,16 @@ try:
 except:
     ref = raw_input("Introduce Fasta file as reference: ")
 
+try:
+    prot = sys.argv[4]
+except:
+    prot = raw_input("Introduce protocol name (miramito/quickmito): ")
+
 manifest = """echo "\n#manifest file for basic mapping assembly with illumina data using MIRA 4\n\nproject = initial-mapping-testpool-to-Salpinus-mt\n\njob=genome,mapping,accurate\n\nparameters = -NW:mrnl=0 -AS:nop=1 SOLEXA_SETTINGS -CO:msr=no\n\nreadgroup\nis_reference\ndata = reference.fa\nstrain = Salpinus-mt-genome\n\nreadgroup = reads\ndata = reads.fastq\ntechnology = solexa\nstrain = testpool\n" > manifest.conf"""
 
 miramito = """mira manifest.conf && MITObim_1.8.pl --clean -start 1 -end 1000 -sample testpool -ref Salpinus_mt_genome -readpool reads.fastq -maf initial-mapping-testpool-to-Salpinus-mt_assembly/initial-mapping-testpool-to-Salpinus-mt_d_results/initial-mapping-testpool-to-Salpinus-mt_out.maf > log"""
+
+quickmito = """MITObim_1.8.pl -start 1 -end 1000 -sample testpool -ref Salpinus_mt_genome -readpool reads.fastq --quick reference.fa --clean > log"""
 
 miramitoout = """/testpool-Salpinus_mt_genome_assembly/testpool-Salpinus_mt_genome_d_results/testpool-Salpinus_mt_genome_out_testpool.unpadded.fasta"""
 
@@ -41,16 +48,21 @@ for npair in range(0,npairs):
         name = ".".join(paironesplit[0:-2])
     elif paironesplit[-1] == "fastq" or paironesplit[-1] == "fq":
         name = ".".join(paironesplit[0:-1])
-    call("mkdir %s" % name , shell=True)
-    os.chdir(name)
+    name = name[:-2]
+    foldername = "%s_%s" % (name,prot)
+    call("mkdir %s" % foldername , shell=True)
+    os.chdir(foldername)
 
     call("seqtk sample -s100 ../%s %s > %s" % (pairone,nreads,name+".fq.subset1"), shell=True)
     call("seqtk sample -s100 ../%s %s > %s" % (pairtwo,nreads,name+".fq.subset2"), shell=True)
     call("shuffleSequences_fastq.pl %s %s %s" % (name+".fq.subset1",name+".fq.subset2",name+".shuffled.fastq"), shell=True)
     call("ln -sf %s reads.fastq" % (name+".shuffled.fastq"), shell=True)
     call("ln -sf ../%s reference.fa" % ref, shell=True)
-    call(manifest, shell=True)
-    call(miramito, shell=True)
+    if prot == "miramito":
+        call(manifest, shell=True)
+        call(miramito, shell=True)
+    elif prot == "quickmito":
+        call(quickmito, shell=True)
     list_dir = os.listdir(".")
     list_dir.sort()
     iterations = []
@@ -58,11 +70,13 @@ for npair in range(0,npairs):
         if dir.startswith("iteration"):
             iterations.append(dir)
     os.chdir("../")
-    consensus = "%s/%s" % (name,iterations[-1]+miramitoout)
+    consensus = "%s/%s" % (foldername,iterations[-1]+miramitoout)
     secus = SeqIO.parse(open(consensus), "fasta")
-    out = open(name+"_consensus.fa", "w")
+    out = open("%s_%s.fa" % (name,prot), "w")
+    i = 0
     for secu in secus:
+        i+=1
         s = str(secu.seq)
         s = s.replace("x","n")
-        out.write(">%s\n%s\n" % (str(secu.id), s))
+        out.write(">%s_%s_%s\n%s\n" % (name,prot,i, s))
     out.close()
