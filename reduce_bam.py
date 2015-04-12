03de39e2128e429bb8b684dce0b5403b93f30b45
fjruizruano <fjruizruano@ugr.es>
2015-04-12 15:37:51 +0200

    update reduce_bam

--- reduce_bam.py
+++ reduce_bam.py
@@ -3,10 +3,17 @@
 from subprocess import call
 import sys
 
+print "Usage: reduce_bam.py BamFile"
+
 try:
     file = sys.argv[1]
 except:
     file = raw_input("Introduce name of your bam file: ")
+
+name = file.split(".")
+name = name[0]
+
+print "\nReducing file %s..." % (file)
 
 #to get all the reads where both mapped.
 call("samtools view -b -F 12 %s > temp1.bam" % file, shell=True)
@@ -18,9 +25,9 @@ call("samtools view -b -f 4 -F 8 %s > temp2.bam" % file, shell=True)
 call("samtools view -b -f 8 -F 4 %s > temp3.bam" % file, shell=True)
 
 #to merge bam files and sort by name
-call("samtools merge -u - temp[123].bam | samtools sort - mapped", shell=True)
+call("samtools merge -u - temp[123].bam | samtools sort - %s_mapped" % (name), shell=True)
 
 #to index sorted bam file
-call("samtools index mapped.bam", shell=True)
+call("samtools index %s_mapped.bam" % (name), shell=True)
 
 call("rm temp1.bam temp2.bam temp3.bam", shell=True)
