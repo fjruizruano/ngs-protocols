e21dcebc62f1c7f2865f6be5ec8d5f981c5f5d7b
fjruizruano <fjruizruano@ugr.es>
2015-04-12 15:38:57 +0200

    bwa for lists

--- bwa_protocol.py
+++ bwa_protocol.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python
 
+import sys
 from subprocess import call
 
-ref = raw_input("FASTA reference file: ")
-lib1 = raw_input("FASTQ read library 1 file 1: ")
-lib2 = raw_input("FASTQ read library 2 file 1: ")
-threads = raw_input("number of threads: ")
+print "\nUsage: bwa_protocol.py ListOfReads Reference Threads [noreduce/reduce]\n"
+
+try:
+    data = sys.argv[1]
+except:
+    data = raw_input("List of reads: ")
+
+try:
+    ref = sys.argv[2]
+except:
+    ref = raw_input("FASTA reference file: ")
+
+try: 
+    threads = sys.argv[3]
+except:
+    threads = raw_input("Number of threads: ")
+
+try:
+    reduce = sys.argv[4]
+except:
+    reduce = raw_input("Reduce or not reduce: ")
+
+files = open(data).readlines()
+l_files = []
+for f in range(0,(len(files)/2)-1):
+    l_files.append([files[f*2][:-1],files[(f*2)+1][:-1]])
 
 try:
 	open(ref+".pac")
@@ -16,14 +39,17 @@ try:
 except:
 	call("bwa index -a bwtsw %s" % ref, shell=True)
 
-call("bwa aln -t%s %s %s > read1.sai" % (threads, ref, lib1), shell=True)
-
-call("bwa aln -t%s %s %s > read2.sai" % (threads, ref, lib2), shell=True)
-
-call("bwa sampe -s -r \042@RG\tID:1\tLB:1\tSM:1\042 %s read1.sai read2.sai %s %s | samtools view -bS - > align_fastq.bam" % (ref, lib1, lib2), shell=True)
-
-call("samtools sort align_fastq.bam align_sort", shell=True)
-
-call("samtools index align_sort.bam", shell=True)
-
-call("samtools flagstat align_sort.bam > align_sort.flagstat", shell=True)
+for pair in l_files:
+    name = pair[0]
+    name = name.split(".")
+    name = name[0][:-2]
+    call("bwa aln -t%s %s %s > read1.sai" % (threads, ref, pair[0]), shell=True)
+    call("bwa aln -t%s %s %s > read2.sai" % (threads, ref, pair[1]), shell=True)
+    call("bwa sampe -s -r \042@RG\tID:1\tLB:1\tSM:1\042 %s read1.sai read2.sai %s %s | samtools view -bS - > %s_fastq.bam" % (ref, pair[0], pair[1], name), shell=True)
+    call("rm read1.sai read2.sai", shell=True)
+    call("samtools sort %s_fastq.bam %s_sort" % (name, name), shell=True)
+    call("samtools index align_sort.bam", shell=True)
+    call("rm %s_fastq.bam", shell=True)
+    call("samtools flagstat %s_sort.bam > %s_sort.flagstat" % (name, name), shell=True)
+    if reduce == "reduce":
+        call("reduce_bam.py %s_sort.bam && rm %s_sort.bam" % (name, name), shell=True)
