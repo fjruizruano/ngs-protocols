f566aa5ec4b1d5eff79d20645646be49a6472f33
fjruizruano <fjruizruano@ugr.es>
2015-04-19 16:48:06 +0200

    ssaha2_run cleans intermediate file

--- ssaha2_run.py
+++ ssaha2_run.py
@@ -40,7 +40,11 @@ for n in range(0,len(files)/2):
     file2 = files[(n*2)+1][:-1]
 #    filepoints
 
-    call("ssaha2 -solexa  -pair 20,400 -cmatch 40 -identity 80 -output sam -outfile %s -best 1 -save %s %s %s" % (file1+".sam",refname,file1,file2), shell=True)
+#    call("ssaha2 -solexa  -pair 20,400 -identity 80 -output sam -outfile %s -best 1 -save %s %s %s" % (file1+".sam",refname,file1,file2), shell=True)
+    call("ssaha2 -solexa  -pair 20,400 -score 40 -identity 80 -output sam -outfile %s -best 1 -save %s %s %s" % (file1+".sam",refname,file1,file2), shell=True)
     call("samtools view -bt %s.fai %s > %s" % (ref,file1+".sam",file1+".bam"), shell=True)
+    call("rm %s" % (file1+".sam"), shell=True)
     call("samtools sort %s %s" % (file1+".bam", file1+".sort"), shell=True)
+    call("rm %s" % (file1+".bam"), shell=True)
     call("reduce_bam.py %s" % (file1+".sort.bam"), shell=True)
+    call("rm %s" % (file1+".sort.bam"), shell=True)
