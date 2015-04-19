1ee88005e45b6704c4329596bc22719f656ac5af
fjruizruano <fjruizruano@ugr.es>
2015-04-19 22:10:26 +0200

    added dnapipete_createdb

---
+++ dnapipete_createdb.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import sys
+from Bio import SeqIO
+
+print "Usage: dnapipete_createdb.py Trinity.fasta one_RM_hit_per_Trinity_contigs"
+
+try:
+    trinity = sys.argv[1]
+except:
+    trinity = raw_input("Introduce path the Trinity.fasta file: ")
+
+try:
+    annot = sys.argv[2]
+except:
+    annot = raw_input("Introduce path to the one_RM_hit_per_Trinity_contigs file: ")
+
+secus = SeqIO.parse(open(trinity),"fasta")
+annot_read = open(annot).readlines()
+annotations = {}
+
+for annotation in annot_read:
+    annotation = annotation.split()
+    annotations[annotation[0]] = annotation[4]
+
+w = open("Trinity.annot.fasta","w")
+
+for sec in secus:
+    name = ""
+    if sec.id in annotations:
+        name = "%s#%s" % (sec.id, annotations[sec.id])
+    else:
+        name = "%s#Unknown" % (sec.id)
+
+    w.write(">%s\n%s\n" % (name, str(sec.seq)))
+
+w.close()
