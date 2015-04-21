a0a621debae3f19ec9744b31ac10859e38a797f0
fjruizruano <fjruizruano@ugr.es>
2015-04-21 10:20:46 +0200

    added gzip support to ssaha2_run

--- ssaha2_run.py
+++ ssaha2_run.py
@@ -38,13 +38,33 @@ files = open(files).readlines()
 for n in range(0,len(files)/2):
     file1 = files[n*2][:-1]
     file2 = files[(n*2)+1][:-1]
-#    filepoints
+    ext1 = file1.split(".")
+    if ext1[-1] == "gz":
+        file1_n = ext1[0:-1]
+        file1_n = ".".join(file1_n)
+        print "Uncompressing file %s" % file1
+        call("seqtk seq %s > %s" % (file1, file1_n), shell=True)
+        file1 = file1_n
+    ext2 = file2.split(".")
+    if ext2[-1] == "gz":
+        file2_n = ext2[0:-1]
+        file2_n = ".".join(file2_n)
+        print "Uncompressing file %s" % file2
+        call("seqtk seq %s > %s" % (file2, file2_n), shell=True)
+        file2 = file2_n
 
 #    call("ssaha2 -solexa  -pair 20,400 -identity 80 -output sam -outfile %s -best 1 -save %s %s %s" % (file1+".sam",refname,file1,file2), shell=True)
+    print "Running SSAHA2"
     call("ssaha2 -solexa  -pair 20,400 -score 40 -identity 80 -output sam -outfile %s -best 1 -save %s %s %s" % (file1+".sam",refname,file1,file2), shell=True)
+    if ext1[-1] == "gz":
+        call("rm %s" % (file1))
+    if ext2[-1] == "gz":
+        call("rm %s" % (file2))
+    print "Generating BAM file"
     call("samtools view -bt %s.fai %s > %s" % (ref,file1+".sam",file1+".bam"), shell=True)
     call("rm %s" % (file1+".sam"), shell=True)
     call("samtools sort %s %s" % (file1+".bam", file1+".sort"), shell=True)
     call("rm %s" % (file1+".bam"), shell=True)
+    print "Reducing BAM file"
     call("reduce_bam.py %s" % (file1+".sort.bam"), shell=True)
     call("rm %s" % (file1+".sort.bam"), shell=True)
