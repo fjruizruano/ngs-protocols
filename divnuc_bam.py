85c549f1bf4793b60fa6991c797e8829f679ea0f
fjruizruano <fjruizruano@ugr.es>
2015-04-21 10:54:04 +0200

    divnuc joined to divnuc_bam

--- divnuc_bam.py
+++ divnuc_bam.py
@@ -69,4 +69,41 @@ for l in ab_dict_sorted:
 
 w.close()
 
-call("./divnuc.py %s.fixed" % (bam), shell=True)
+table = bam+".fixed"
+
+data = open(table).readlines()
+
+def divnuc(seq):
+    total = 0
+    dif = 0
+    a = 0
+    for i in range(0,len(seq)-1):
+        a += 1
+        for j in range(a,len(seq)):
+            total += 1
+            if seq[i] != seq[j]:
+                dif += 1
+    try:
+        diversity = round(1.0*dif/total, 100)
+    except:
+        diversity = 0
+    return diversity
+
+nucs = ["D","I","A", "C", "T", "G"]
+
+w = open(table+".divnuc", "w")
+
+counter = 0
+for line in data:
+    info = line.split()
+    seq = ""
+    for nuc in range(0,len(nucs)):
+        seq = seq +(nucs[nuc] * int(info[nuc+3]))
+    res = divnuc(seq)
+    counter += 1
+    print str(counter) + " - " + str(res)
+    info.append(res)
+    info = [str(x) for x in info]
+    w.write("\t".join(info)+"\n")
+
+w.close()
