f1192b435adce60fc7083506daf5deeab79fc9b7
fjruizruano <fjruizruano@ugr.es>
2015-06-24 15:45:28 +0200

    rm_join_out.py added

---
+++ rm_join_out.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python
+
+files = open("lista_out.txt")
+
+# process RM *.out file
+dict_count = {}
+dict_diver = {}
+
+header = """   SW   perc perc perc  query                                        position in query    matching             repeat        position in repeat
+score   div. del. ins.  sequence                                     begin end   (left)   repeat               class/family begin  end    (left)  ID
+
+"""
+
+outout = open("test.all.out", "w")
+outout.write(header)
+
+def process_out(file):
+        file = open(file).readlines()
+
+        for line in file[3:]:
+                outout.write(line)
+                text = line.split()
+                begin = int(text[5])-1
+                end = int(text[6])
+                if end - begin > 50:
+                        diver = float(text[1])
+                        annot = text[9]
+                        look = annot in dict_count
+                        if look == True:
+                                dict_count[annot] += 1
+                                dict_diver[annot].append(diver)
+                        elif look == False:
+                                dict_count[annot] = 1
+                                dict_diver[annot] = [diver]
+
+for file in files:
+        process_out(file[:-1]) #complete dictionary of counts
+
+outout.close()
+
