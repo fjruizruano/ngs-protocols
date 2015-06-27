4021fbcf474668fcc516902978d82f9c23d187a6
fjruizruano <fjruizruano@ugr.es>
2015-06-27 16:06:04 +0200

    trinotate auto with trinotate support

--- trinotate_auto.py
+++ trinotate_auto.py
@@ -3,7 +3,7 @@
 import sys
 from subprocess import call
 
-print "Usage: trinotate_auto.py FastaFile NumberOfThreads DataBase(su) Analysis(xph) [MinimumLenghtCDS]"
+print "Usage: trinotate_auto.py FastaFile NumberOfThreads Analyses(xph) DataBase(su) [MinimumLenghtCDS]"
 
 try:
     transcripts = sys.argv[1]
@@ -17,14 +17,17 @@ except:
     threads = raw_input("Intruduce number of threads (integer): ")
 
 try:
-    db = sys.argv[3]
-except:
-    db = raw_input("Introduce databases (s=swissprot, u=uniref90, p=pfam, su, sp, up): ")
-
-try:
-    analyses = sys.argv[4]
+    analyses = sys.argv[3]
 except:
     analyses = raw_input("Introduce analyses (x=blastx, p=blastp, h=hammer, xp, xh, ph, xph): ")
+
+try:
+    db = sys.argv[4]
+except:
+    if "h" not in analyses:
+        db = raw_input("Introduce databases (s=swissprot, u=uniref90, su): ")
+    else:
+        pass
 
 try:
     cdslen = sys.argv[5]
@@ -32,26 +35,50 @@ try:
 except:
     tc = "100"
 
+tex = "/usr/local/lib/Trinotate-2.0.2/Trinotate"
+tdb = "/mnt/disk2/trinotate/trinotate_db/Trinotate.sqlite.gz"
+
 s = "/mnt/disk2/trinotate/swissprot/uniprot_sprot.trinotate_v2.0.pep"
 u = "/mnt/disk2/trinotate/uniref90/uniprot_uniref90.trinotate_v2.0.pep"
 h = "/mnt/disk2/trinotate/pfam/Pfam-A.hmm"
 db_dict = {"s": s, "u":u, "h":h}
 
+try:
+    ttt = open("Trinotate.sqlite")
+except:
+    call("gunzip -c %s > Trinotate.sqlite" % (tdb), shell=True)
+
 if "p" in analyses or "h" in analyses:
-    call("TransDecoder.LongOrfs -t %s -m %s" % (transcripts,cdslen), shell=True)
     pep = transcripts + ".transdecoder_dir/longest_orfs.pep"
+    try:
+        ttt = open(pep)
+    except:
+        call("TransDecoder.LongOrfs -t %s -m %s" % (transcripts,cdslen), shell=True)
 
 if "x" in analyses:
     for d in db:
         print "Running BLASTX with " + db_dict[d]
-        call("blastx -query %s -db %s -num_threads %s -max_target_seqs 1 -outfmt 6 > %s.%s.blastx.outfmt6" % (transcripts,db_dict[d],threads,transcripts,d) , shell=True)
+        outfile = "%s.%s.blastx.outfmt6" % (transcripts,d)
+        call("blastx -query %s -db %s -num_threads %s -max_target_seqs 1 -outfmt 6 > %s" % (transcripts,db_dict[d],threads,outfile) , shell=True)
+        if d == "s":
+            call("%s Trinotate.sqlite LOAD_swissprot_blastx %s" % (tex,outfile), shell=True)
+        elif d == "u":
+            call("%s Trinotate.sqlite LOAD_trembl_blastx %s" % (tex,outfile), shell=True)
 
 if "p" in analyses:
     for d in db:
         print "Running BLASTP with " + db_dict[d]
-        call("blastp -query %s -db %s -num_threads %s -max_target_seqs 1 -outfmt 6 > %s.%s.blastp.outfmt6" % (pep,db_dict[d],threads,transcripts,d) , shell=True)
+        outfile = "%s.%s.blastp.outfmt6" % (transcripts,d)
+        call("blastp -query %s -db %s -num_threads %s -max_target_seqs 1 -outfmt 6 > %s" % (pep,db_dict[d],threads,outfile), shell=True)
+        if d == "s":
+            call("%s Trinotate.sqlite LOAD_swissprot_blastx %s" % (tex,outfile), shell=True)
+        elif d == "u":
+            call("%s Trinotate.sqlite LOAD_trembl_blastx %s" % (tex,outfile), shell=True)
 
 if "h" in analyses:
     print "Running HMMER with " + h
     call("hmmscan --cpu %s --domtblout %s.pfam.out %s %s > pfam.log" % (threads,transcripts,h,pep), shell=True)
+    call("%s Trinotate.sqlite LOAD_pfam %s.pfam.out" % (tex, transcripts), shell=True)
+
+call("%s Trinotate.sqlite report > %s_annot.xls" % (tex,transcripts), shell=True)
 
