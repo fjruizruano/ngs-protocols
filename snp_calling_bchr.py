10b2d7e384c8d3ad04f628e2437da86ba0c63c78
fjruizruano <fjruizruano@ugr.es>
2016-04-01 03:45:31 +0200

    snp_calling_bchr.py updated

--- snp_calling_bchr.py
+++ snp_calling_bchr.py
@@ -2,34 +2,99 @@
 
 import sys
 import operator
+from subprocess import call
 
-print "Usage: snp_calling_bchr.py VariationFile"
+print "Usage: snp_calling_bchr.py BamSampleFile VariationFile"
 
 try:
-    file = sys.argv[1]
+    samples = sys.argv[1]
+except:
+    samples = raw_input("Introduce file with BAM files and sample information: ")
+
+try:
+    file = sys.argv[2]
 except:
     file = raw_input("Introduce variation file: ")
+
+sample_dict = {}
+sample_list = []
+
+samples = open(samples).readlines()
+
+for s in samples:
+    info = s.split()
+    name = info[0]
+    name = name.split(".")
+    name = ".".join(name[:-1])
+    type = info[1]
+    sample_list.append(name)
+    sample_dict[type] = name
 
 table = open(file).readlines()
 
 w = open("snps_selected.txt", "w")
-w.write("".join(table[0:2]))
+w.write("\t\t"+"\t\t".join(sample_list)+"\t\n")
+w.write("sequence\tpos\t"+"\t".join(["Ref","Alt"]*len(sample_list))+"\n")
 
-var = ["D","I","A","C","T","G"]
+n_samples = len(sample_list)
+
+dictio = {}
 
 for line in table[2:]:
     info = line.split()
-    count = [int(x) for x in info[2:8]]
-    variants = {}
-    for i in range(0,6):
-        variants[var[i]] = count[i]
-    sorted_variants = sorted(variants.items(), key=operator.itemgetter(1))
-    sorted_variants.reverse()
-    one = sorted_variants[0] 
-    two = sorted_variants[1]
-    total = [i[1] for i in sorted_variants]
-    total = sum(total)
-    if total != 0 and 1.0*(two[1]-1)/total > 0.1:
-        w.write("\t".join(info)+"\n")
-        
+    seq = info[0]
+    pos = info[1]
+    var = {}
+    for i in range(0,len(sample_list)):
+        index = (i*6)+2
+        counts = [int(x) for x in info[index:index+6]]
+        var[sample_list[i]] = counts
+    dictio[seq+"."+pos] = var
+
+ref_dict = {}
+
+#keys = {1:"D",2:"I",3:"A",4:"C",5:"T",6:"G"}
+
+for el in dictio:
+    counts = dictio[el]["gdna_zerob"]
+    total = sum(counts)
+    for i in range(0,len(counts)):
+        if counts[i] == total:
+            ref_dict[el] = i
+
+alt_dict = {}
+
+for el in ref_dict:
+    ref = ref_dict[el]
+    counts = dictio[el]["gdna_plusb"]
+    print counts
+    counts_exc = counts[:ref] + [0] + counts[ref+1:]
+    maxim = max(counts_exc)
+    print counts_exc
+    for i in range(0,len(counts)):
+        if counts_exc[i] == maxim and maxim > 3:
+            alt_dict[el] = i
+#            print counts_exc[i]
+print ref
+print counts
+print counts_exc
+print maxim
+
+for el in alt_dict:
+    sel = dictio[el]
+    ref = ref_dict[el]
+    alt = alt_dict[el]
+    seq_name = el.split(".")
+    seq_name = "\t".join(seq_name)
+    final_list = []
+    for s in sample_list:
+        r = sel[s][ref] 
+        a = sel[s][alt]
+        final_list.append(str(r))
+        final_list.append(str(a))
+    w.write("%s\t%s\n" % (seq_name,"\t".join(final_list)))
+
 w.close()
+
+call("(head -n 2 snps_selected.txt && tail -n +3 snps_selected.txt | sort -k 1,1 -k 2n) > snps_selected2.txt ", shell=True)
+
