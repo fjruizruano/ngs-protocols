2cb37d95cffdd6c6e4b5dbcf50f79736fdb9e15b
fjruizruano <fjruizruano@ugr.es>
2017-09-08 20:07:15 +0200

    updating bam_consensus.py

--- bam_consensus.py
+++ bam_consensus.py
@@ -12,28 +12,35 @@ except:
 
 vardata = open(varfile).readlines()
 
-keys = {0:"N",1:"N",2:"A",3:"C",4:"T",5:"G"}
+header = vardata[0]
+samples = header.split()
 
-seq_dict = {}
+keys = {0:"N",1:"N",2:"A",3:"C",4:"T",5:"G"}
 
 out = open("output.txt", "w")
 
-for line in vardata[2:]:
+for m in range(0,len(samples)):
+  seq_dict = {}
+  begin = 2+(m*6)
+  end = 2+(m*6)+6
+  for line in vardata[2:]:
     data = line.split()
-    nums = [int(x) for x in data[2:]]
+    nums = [int(x) for x in data[begin:end]]
     nums_dict = {}
     for n in range(0,6):
         nums_dict[n] = nums[n]
     sorted_nums = sorted(nums_dict.items(), key=operator.itemgetter(1))
     nucleotide = sorted_nums[5][0]
+    abundance = sorted_nums[5][1]
+    if abundance == 0:
+        nucleotide = 0 # nucleotide is an "N"
     base = keys[nucleotide]
     if data[0] in seq_dict:
         seq_dict[data[0]].append(base)
     else:
         seq_dict[data[0]] = [base]
-
-for el in seq_dict:
-    out.write(">%s\n" % (el))
+  for el in seq_dict:
+    out.write(">%s_%s\n" % (samples[m],el))
     out.write("".join(seq_dict[el]))
     out.write("\n")
 
