136ba4e2beb8f3390086e48a58e5c77b9e250e2e
fjruizruano <fjruizruano@ugr.es>
2018-02-04 12:37:18 +0100

    adding scripts to work with the RepeatMasker annotations

---
+++ rm_count_matches_monomers.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import sys
+from Bio import SeqIO
+
+print "Usage: rm_count_matches.py FastaFile\n"
+
+try:
+    fasta = sys.argv[1]
+except:
+    fasta = raw_input ("Introduce FastaFile: ")
+
+seqs = SeqIO.parse(open(fasta),"fasta")
+
+counter_dim = {}
+counter_nodim = {}
+
+for seq in seqs:
+    secu = str(seq.seq)
+    id = str(seq.id)
+    info = id.split("_")
+    read = info[1]
+    annot = info[0]
+#    mono_len = annot
+#    mono_len = mono_len.split("-")
+#    mono_len = int(mono_len[1])
+
+    if annot not in counter_dim:
+        counter_dim[annot] = [0,0]
+        counter_nodim[annot] = [0,0]
+
+    if len(secu) > 100:
+        a = counter_dim[annot][0]
+        b = counter_dim[annot][1]
+        counter_dim[annot] = [a+1,b+len(secu)]
+    else:
+        a = counter_nodim[annot][0]
+        b = counter_nodim[annot][1]
+        counter_nodim[annot] = [a+1,b+len(secu)]
+
+out = open(fasta+".counts", "w")
+out.write("Annotation\tDIM_N\tDIM_MON\tNODIM_N\tNODIM_MON\n")
+
+for el in counter_dim:
+    dim = counter_dim[el]
+    nodim = counter_nodim[el]
+    lili = dim+nodim
+    lili = [str(i) for i in lili]
+    lili = [el]+lili
+    out.write("\t".join(lili)+"\n")
