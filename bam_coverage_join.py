909501fd603eea75cf5db1705361790b0dce5d57
fjruizruano <fjruizruano@ugr.es>
2018-05-03 17:21:01 +0200

    bam_coverage_join.py recognizes var files

--- bam_coverage_join.py
+++ bam_coverage_join.py
@@ -2,6 +2,8 @@
 
 import sys
 from subprocess import call
+from os import listdir
+from os.path import isfile, join
 
 print "bam_coverage_join.py FastaReference ListOfBams [Maximum Depth (Default: 8000)]"
 
@@ -24,14 +26,24 @@ bams = open(li).readlines()
 
 to_join = []
 
+onlyfiles = [f for f in listdir(".") if isfile(join(".",f))]
+
 for bam in bams:
-    bam = bam[:-1]    
-    call("pysamstats --max-depth=%s -f %s --type variation %s > %s.var" % (depth, ref,bam,bam), shell=True)
-    call("""awk {'print $1"popeye"$2"\t"$4'} %s.var > %s.var2""" % (bam,bam), shell=True)
+    bam = bam[:-1]
+    if bam+".var2" not in onlyfiles:
+        if bam+".var" not in onlyfiles:
+            print "Not found file %s.var, generating..." % bam
+            call("pysamstats --max-depth=%s -f %s --type variation %s > %s.var" % (depth, ref,bam,bam), shell=True)
+        call("""awk {'print $1"popeye"$2"\t"$4'} %s.var > %s.var2""" % (bam,bam), shell=True)
+        print "Not found file %s.var2, generating..." % bam
+    else:
+        print "Found file %s.var2, skipping."
     to_join.append(bam+".var2")
 
+print "Joining lists..."
 call("join_multiple_lists.py %s " % " ".join(to_join), shell=True)
 
 call("""sed 's/popeye/\t/g' toico.txt > toico2.txt""", shell=True)
 
 call("(head -n 1 toico2.txt && tail -n +2 toico2.txt | sort -k 1,1 -k 2n) > toico3.txt ", shell=True)
+Print "We're done!!!\n"
