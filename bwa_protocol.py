4a77afe85b6440064662548695fc0b0228aee6ed
fjruizruano <fjruizruano@ugr.es>
2018-08-16 12:24:09 +0200

    updating bwa_protocol.py

--- bwa_protocol.py
+++ bwa_protocol.py
@@ -45,10 +45,11 @@ for pair in l_files:
     name = name[0][:-2]
     call("bwa aln -t%s %s %s > read1.sai" % (threads, ref, pair[0]), shell=True)
     call("bwa aln -t%s %s %s > read2.sai" % (threads, ref, pair[1]), shell=True)
-    call("bwa sampe -s -r \042@RG\tID:1\tLB:1\tSM:1\042 %s read1.sai read2.sai %s %s | samtools view -bS - > %s_fastq.bam" % (ref, pair[0], pair[1], name), shell=True)
+    call("bwa sampe %s read1.sai read2.sai %s %s | samtools view -bS - > %s_fastq.bam" % (ref, pair[0], pair[1], name), shell=True)
     call("rm read1.sai read2.sai", shell=True)
-    call("samtools sort %s_fastq.bam %s_sort" % (name, name), shell=True)
+    call("samtools sort -T aln.sorted %s_fastq.bam -o %s_sort.bam" % (name, name), shell=True)
     call("rm %s_fastq.bam" % (name), shell=True)
+    call("samtools index %s_sort.bam" % (name), shell=True)
     call("samtools flagstat %s_sort.bam > %s_sort.flagstat" % (name, name), shell=True)
     if reduce == "reduce":
         call("reduce_bam.py %s_sort.bam && rm %s_sort.bam" % (name, name), shell=True)
