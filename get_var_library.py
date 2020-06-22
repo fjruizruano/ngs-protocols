51b2059aae5e10e17cd6a1f2c8b7cf1465446b5c
Paquillo <fjruizruano@ugr.es>
2020-06-22 22:18:13 +0200

    Updated get_var_library

--- get_var_library.py
+++ get_var_library.py
@@ -3,6 +3,11 @@
 ra = open("ref_alt3.txt").readlines()
 var = open("toico4.txt").readlines()
 
+header = var[0]
+samples = header.split()
+n_samples = len(samples)
+n_samples = (n_samples-2)/6
+
 w = open("out.txt","w")
 
 ra_list = []
@@ -17,7 +22,7 @@ for i in range(0,len(ra)):
     var_line = var[i]
     var_info = var_line.split()
     res = []
-    for j in range(0,22):
+    for j in range(0,n_samples):
         r_num = 2+(j*6)+r
         a_num = 2+(j*6)+a
         res.append(var_info[r_num])
